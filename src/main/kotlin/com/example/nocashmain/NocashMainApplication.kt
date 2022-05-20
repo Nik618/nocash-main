6c72c8e049e90678b21ded587815516735e8fe1e
Nik618 <trinax01@gmail.com>
2022-05-20 14:48:08 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/nocashmain/NocashMainApplication.kt
@@ -0,0 +1,11 @@
+package com.example.nocashmain
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NocashMainApplication
+
+fun main(args: Array<String>) {
+    runApplication<NocashMainApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/nocashmain/NocashMainApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.nocashmain
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NocashMainApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
