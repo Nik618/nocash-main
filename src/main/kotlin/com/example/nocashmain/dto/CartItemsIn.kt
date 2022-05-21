feef09b3fbac1179e1ba1eff95d641420d3752e2
Nik618 <trinax01@gmail.com>
2022-05-22 00:10:34 +0300

    polufinal 32

---
+++ src/main/kotlin/com/example/nocashmain/dto/CartItemsIn.kt
@@ -0,0 +1,11 @@
+package com.example.nocashmain.dto
+
+import com.example.nocashmain.entity.ProductEntity
+import com.example.nocashmain.entity.UserEntity
+
+data class CartItemsIn (
+    var id: Int? = null,
+    var idUser : Int? = null,
+    var idProduct : Int? = null,
+    var count : Int? = null
+)
