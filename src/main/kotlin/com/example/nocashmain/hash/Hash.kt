64fb4c93a72faf9d512c2df439567c040f672511
Nik618 <trinax01@gmail.com>
2022-05-21 10:37:56 +0300

    create hash

---
+++ src/main/kotlin/com/example/nocashmain/hash/Hash.kt
@@ -0,0 +1,38 @@
+package com.example.nocashmain.hash
+
+import java.security.NoSuchAlgorithmException
+import java.security.spec.InvalidKeySpecException
+import java.util.*
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
+
+class Hash {
+    private val ITERATIONS = 65536
+    private val KEY_LENGTH = 512
+    private val ALGORITHM = "PBKDF2WithHmacSHA512"
+
+    fun hashPassword(password: String, salt: String): Optional<String>? {
+        val chars = password.toCharArray()
+        val bytes = salt.toByteArray()
+        val spec = PBEKeySpec(chars, bytes, ITERATIONS, KEY_LENGTH)
+        Arrays.fill(chars, Character.MIN_VALUE)
+        return try {
+            val fac = SecretKeyFactory.getInstance(ALGORITHM)
+            val securePassword = fac.generateSecret(spec).encoded
+            Optional.of(Base64.getEncoder().encodeToString(securePassword))
+        } catch (ex: NoSuchAlgorithmException) {
+            System.err.println("Exception encountered in hashPassword()")
+            Optional.empty()
+        } catch (ex: InvalidKeySpecException) {
+            System.err.println("Exception encountered in hashPassword()")
+            Optional.empty()
+        } finally {
+            spec.clearPassword()
+        }
+    }
+
+    fun verifyPassword(password: String?, key: String?, salt: String?): Boolean {
+        val optEncrypted: Optional<String>? = hashPassword(password!!, salt!!)
+        return if (!optEncrypted!!.isPresent) false else optEncrypted.get() == key
+    }
+}
