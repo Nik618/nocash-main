5d372efe73fa72bb50132ee2af7dafa9acd47bbb
Nik618 <trinax01@gmail.com>
2022-05-21 19:29:38 +0300

    polufinal 19

--- src/main/kotlin/com/example/nocashmain/service/OrderService.kt
+++ src/main/kotlin/com/example/nocashmain/service/OrderService.kt
@@ -47,6 +47,7 @@ class OrderService {
             paymentId = order.paymentId
         }
 
+        orderRepository.save(orderEntity)
         //val cartItemsEntity = cartItemsRepository.findByIdUser(transactionRepository.findById(order.idTransaction!!).get().idUserFrom?.id!!).get(0)
 
         val cartItemsEntities = cartItemsRepository.findAllByIdUser(transactionRepository.findById(order.idTransaction!!).get().idUserTo!!)
