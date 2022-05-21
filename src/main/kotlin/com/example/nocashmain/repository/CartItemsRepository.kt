bf29c0a9986564145c40494a21bdb50adbce5281
Nik618 <trinax01@gmail.com>
2022-05-21 18:41:37 +0300

    polufinal 14

--- src/main/kotlin/com/example/nocashmain/repository/CartItemsRepository.kt
+++ src/main/kotlin/com/example/nocashmain/repository/CartItemsRepository.kt
@@ -3,11 +3,12 @@ package com.example.nocashmain.repository
 import com.example.nocashmain.entity.CartItemsEntity
 import com.example.nocashmain.entity.CategoryEntity
 import com.example.nocashmain.entity.ProductEntity
+import com.example.nocashmain.entity.UserEntity
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CartItemsRepository : CrudRepository<CartItemsEntity, Int> {
     fun findByIdUser(id : Int): List<CartItemsEntity?>
-    fun findAllByIdUser(id : Int): List<CartItemsEntity?>
+    fun findAllByIdUser(idUser: UserEntity): List<CartItemsEntity?>
 }
